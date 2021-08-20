1f468025f5c9a9e8e75fd08b319fef7a7d72699f
YOSEPHLEE <laksmi0628@gmail.com>
2021-08-20 13:42:26 +0900

    11_원시 값과 객체의 비교

--- example.js
+++ example.js
@@ -1,76 +1,83 @@
+var str = "string";
+
+//문자열은 유사 배열이므로 배열과 유사하게 인덱스를 사용해 각 문자에 접근할 수 있다.
+console.log(str[0]); // s
+
+//원시 값인 문자열이 객체처럼 동작한다.
+console.log(str.length); // 6
+console.log(str.toUpperCase); // STRING
+
+var score = 80;
+
+//copy 변수에는 score 변수의 값 80이 복사되어 할당된다.
+var copy = score;
+
+console.log(score, copy); // 80,80
+console.log(score === copy); //true
+
+//score 변수와 copy 변수의 값은 다른 메모리 공간에 저장된 별개의 값이다.
+// 따라서 score 변수의 값을 변경해도 copy 변수의 값에는 어떠한 영향도 주지 않는다.
+score = 100;
+
+console.log(score, copy); // 100 80
+console.log(score === copy); // false
+
+//할당이 이뤄지는 시점에 객체 리터럴이 해석되고, 그 결과 객체가 생성된다.
 var person = {
   name: "Lee",
-  sayHello: function () {
-    console.log(`Hello! My name is ${this.name}`);
-  },
-};
-console.log(typeof person); // object
-
-var empty = {}; // empty object
-console.log(typeof empty); // object
-
-var person = {
-  firstName: "GORO", // 식별자 네이밍 규칙을 준수하는 키
-  "last-name": "LEE", // 식별자 네이밍 규칙을 준수하지 않는 키
 };
 
-var person = {
-  firstName: "GORO",
-  "last-name": "LEE", // SyntaxError: Unexpected token -
-};
-
-var obj = {};
-var key = "hello";
-
-//ES5
-obj[key] = "world";
-//ES6
-// var obj = {[key]:"world"}
-
-console.log(obj); // {hello : "world"}
-
-var foo = {
-  name: "LEE",
-  name: "Kim",
-};
-console.log(foo); // {name:"KIM"}
+// person 변수에 저장되어 있는 참조 값으로 실제 객체에 접근한다.
+console.log(person);
 
 var person = {
   name: "Lee",
 };
 
-//마침표 표기법
-console.log(person.name);
+//프로퍼티 값 갱신
+person.name = "kim";
 
-//대괄호 표기법
-console.log(person["name"]);
+//프로퍼티 동적 생성
+person.address = "Seoul";
+
+console.log(person); // {name: "kim", adress: "Seoul"}
+
+const o = { x: { y: 1 } };
+
+//얕은 복사
+const c1 = { ...o };
+console.log(c1 === o); // false
+console.log(c1.x === o.x); // true
+
+//깊은 복사
+const c2 = _.cloneDeep(o);
+console.log(c2 === o); // false
+console.log(c2.x === o.x); // false
 
 var person = {
-  name: "LEE",
+  name: "Lee",
 };
-person.age = 20;
-console.log(person); // {name: "LEE", age: 20}
+
+// 참조 값을 복사(얕은 복사)
+var copy = person;
 
 var person = {
-  name: "LEE",
-};
-delete person.name;
-
-//ES6
-let x = 1,
-  y = 7;
-
-const obj = { x, y };
-
-console.log(obj); // {x:1,y:2}
-
-//ES6
-const obj = {
-  name: "LEE",
-  //메서드 축약 표현
-  sayHi() {
-    console.log("hello");
-  },
+  name: "Lee",
 };
 
-obj.sayHi(); // hello
+// 참조 값을 복사(얕은 복사), copy와 person은 동일한 참조 값을 갖는다.
+var copy = person;
+
+// copy와 person은 동일한 객체를 참조한다.
+console.log(copy === person); // true
+
+// copy를 통해 객체를 변경한다.
+copy.name = "Kim";
+
+// person을 통해 객체를 변경한다.
+person.address = "Seoul";
+
+// copy와 person은 동일한 객체를 가리킨다.
+// 따라서 어느 한쪽에서 객체를 변경하면 서로 영향을 주고받는다.
+console.log(person); // {name: "Kim", address: "Seoul"}
+console.log(copy); // {name: "Kim", address: "Seoul"}
